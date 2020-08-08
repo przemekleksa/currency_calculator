4d103fcd4eacde61b19036a8b9528dee2d764ebe
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2020-08-08 14:53:57 +0200

    production build

---
+++ precache-manifest.8e5ccf7a74c5e3f5c22ca0fa946e206b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6350b75c9d63d0c06059e10b2c6687fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcdfb2643cc0605e4987",
+    "url": "/static/css/main.47e637a3.chunk.css"
+  },
+  {
+    "revision": "c81b2ee150018381adeb",
+    "url": "/static/js/2.3af785a5.chunk.js"
+  },
+  {
+    "revision": "fcdfb2643cc0605e4987",
+    "url": "/static/js/main.c052e056.chunk.js"
+  },
+  {
+    "revision": "675ff7799032c9d2d26e",
+    "url": "/static/js/runtime-main.3c844723.js"
+  }
+]);
